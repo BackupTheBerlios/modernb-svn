821678c5b610bdb48c5354c99d4965f9a0ec32f1
fyr <fyr@e3c48d74-9504-0410-90e6-fd4d282aed68>
2005-12-19 14:10:52 +0000

    * version changed * CLS_CONTACTLIST definition

--- trunk/version.h
+++ trunk/version.h
@@ -1,20 +1,20 @@
-#define FILE_VERSION	0, 3, 3, 4
-#define PRODUCT_VERSION	0, 3, 3, 4
+#define FILE_VERSION	0, 4, 1, 1
+#define PRODUCT_VERSION	0, 4, 1, 1
 
 #ifdef UNICODE
 #ifndef _DEBUG
-	#define FILE_VERSION_STR	"0, 3, 3, UNICODE build 4"
-	#define PRODUCT_VERSION_STR	"0, 3, 3, UNICODE build 4"
+	#define FILE_VERSION_STR	"0, 4, 1, UNICODE build 1"
+	#define PRODUCT_VERSION_STR	"0, 4, 1, UNICODE build 1"
 #else
-	#define FILE_VERSION_STR	"0, 3, 3, DEBUG UNICODE build 4"
-	#define PRODUCT_VERSION_STR	"0, 3, 3, DEBUG UNICODE build 4"
+	#define FILE_VERSION_STR	"0, 4, 1, DEBUG UNICODE build 1"
+	#define PRODUCT_VERSION_STR	"0, 4, 1, DEBUG UNICODE build 1"
 #endif
 #else
 #ifndef _DEBUG
-	#define FILE_VERSION_STR	"0, 3, 3, build 4"
-	#define PRODUCT_VERSION_STR	"0, 3, 3, build 4"
+	#define FILE_VERSION_STR	"0, 4, 1, build 1"
+	#define PRODUCT_VERSION_STR	"0, 4, 1, build 1"
 #else
-	#define FILE_VERSION_STR	"0, 3, 3, DEBUG build 4"
-	#define PRODUCT_VERSION_STR	"0, 3, 3, DEBUG build 4"
+	#define FILE_VERSION_STR	"0, 4, 1, DEBUG build 1"
+	#define PRODUCT_VERSION_STR	"0, 4, 1, DEBUG build 1"
 #endif
 #endif
